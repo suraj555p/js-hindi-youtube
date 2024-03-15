e8e9de7e45ecea9944fab450118ebc904ba1a431
suraj555p <121879330+suraj555p@users.noreply.github.com>
2024-03-15 01:57:43 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict "; // treat all JS code as a new version
+
+//alert(3+3) // we are using node JS , not a browser
+
+console.log(3+4)
+console.log("suraj")
+
+let name="suraj"
+let age=23
+let isLoggedIn=false
+let state
+
+// number=> 2 to power 53
+//bigint 
+// string=>""
+//boolean=>true/false
+//null=> standlone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
