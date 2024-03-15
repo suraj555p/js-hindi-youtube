8b2e7793e9d77f481af2832b59647262c10739b4
suraj555p <121879330+suraj555p@users.noreply.github.com>
2024-03-15 01:58:47 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountID=453661
+let accountEmail="suraj8861@gmail.com"
+var accountPassword="12345"
+accountCity="Indore"
+
+//accountID=2
+accountEmail="suraj987"
+accountPassword="9832"
+accountCity="banglore"
+let accountState
+
+console.log(accountID);
+
+/*
+   prefer not to use var
+   because of issue in block scope and function scope 
+*/
+
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
